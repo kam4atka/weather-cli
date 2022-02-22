d6623e0e2e4c0c19add1554db17da734496fb215
kam4atka <sledsib@ya.ru>
2022-02-22 19:23:23 +0300

    Added file processing

---
+++ src/const.js
@@ -0,0 +1,6 @@
+export const NAME_FILE = 'weather-data.json';
+
+export const MESSAGES = {
+  TOKEN_SUCCESS: 'Токен сохранен',
+  TOKEN_ERROR: 'Токен не существует'
+};
--- src/weather.js
+++ src/weather.js
@@ -1,11 +1,30 @@
 import {getObjectOfArgs} from './helpers/args.js';
-import {printHelp} from './services/logger.js';
+import {printError, printHelp, printSuccess} from './services/logger.js';
+import {saveKeyValue} from './services/storage.js';
+import {MESSAGES} from './const.js';
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError(MESSAGES.TOKEN_ERROR);
+    return;
+  }
+
+  try {
+    await saveKeyValue('token', token);
+    printSuccess(MESSAGES.TOKEN_SUCCESS);
+  } catch (err) {
+    printError(err.message);
+  }
+}
 
 const initCLI = () => {
   const args = getObjectOfArgs(process.argv);
 
   if (args.h) {
     printHelp();
+  }
+  if (args.t) {
+    return saveToken(args.t);
   }
 };
 
