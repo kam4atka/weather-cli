4a65d7d03d20c1b2ffcf50dc07045e248e32a56a
kam4atka <sledsib@ya.ru>
2022-02-24 13:29:23 +0300

    Added API processing

--- src/const.js
+++ src/const.js
@@ -1,6 +1,24 @@
 export const NAME_FILE = 'weather-data.json';
 
+export const ENTRIES = {
+  TOKEN: 'token'
+};
+
 export const MESSAGES = {
   TOKEN_SUCCESS: 'Токен сохранен',
-  TOKEN_ERROR: 'Токен не существует'
+  TOKEN_ERROR: 'Не задан ключ API. Воспользуйтесь ключем -h, чтобы узнать подробнее как добавить ключ API'
+};
+
+export const WEATHER_SERVER_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+export const CONFIG_API_PARAMS = {
+  CITY: 'q',
+  TOKEN: 'appid',
+  LANG: 'lang',
+  UNITS: 'units'
+};
+
+export const CONFIG_API_VALUE = {
+  LANG: 'ru',
+  UNITS: 'metric'
 };
---
+++ src/services/api.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import {getKeyValue} from './storage.js';
+import {
+  WEATHER_SERVER_URL,
+  ENTRIES,
+  MESSAGES,
+  CONFIG_API_PARAMS,
+  CONFIG_API_VALUE
+} from '../const.js';
+
+const getWeather = async (city) => {
+  const token = await getKeyValue(ENTRIES.TOKEN);
+
+  if (!token) {
+    throw new Error(MESSAGES.TOKEN_ERROR);
+  }
+
+  const {data} = await axios.get(WEATHER_SERVER_URL, {
+    params: {
+      [CONFIG_API_PARAMS.CITY]: city,
+      [CONFIG_API_PARAMS.TOKEN]: token,
+      [CONFIG_API_PARAMS.LANG]: CONFIG_API_VALUE.LANG,
+      [CONFIG_API_PARAMS.UNITS]: CONFIG_API_VALUE.UNITS
+    }
+  });
+
+  return data;
+
+  // * An alternative option using the https library.
+  // * You need add  next line before:
+  // * import https from 'https';
+
+  // const url = new URL(WEATHER_SERVER_URL);
+  // url.searchParams.append(CONFIG_API_PARAMS.CITY, city);
+  // url.searchParams.append(CONFIG_API_PARAMS.TOKEN, token);
+  // url.searchParams.append(CONFIG_API_PARAMS.LANG, CONFIG_API_VALUE.LANG);
+  // url.searchParams.append(CONFIG_API_PARAMS.UNITS, CONFIG_API_VALUE.UNITS);
+
+  // https.get(url, (response) => {
+  //   let result = '';
+
+  //   response.on('data', (chunk) => {
+  //     result += chunk;
+  //   });
+
+  //   response.on('end', () => {
+  //     console.log(result);
+  //   });
+  // });
+};
+
+export {getWeather};
