ce5a096b7cfe33ae01444bdaedf20eaee450bc4c
kam4atka <sledsib@ya.ru>
2022-02-24 16:50:36 +0300

    Added error processing

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,7 @@
 import {getObjectOfArgs} from './helpers/args.js';
 import {printError, printHelp, printSuccess} from './services/logger.js';
 import {saveKeyValue} from './services/storage.js';
-import {MESSAGES, ENTRIES} from './const.js';
+import {MESSAGES, ENTRIES, HTTP_CODE} from './const.js';
 import {getWeather} from './services/api.js';
 
 const saveToken = async (token) => {
@@ -18,6 +18,30 @@ const saveToken = async (token) => {
   }
 }
 
+const getForcast = async () => {
+  try {
+    const weather = await getWeather('moscow')
+
+    return weather;
+  } catch (err) {
+    if (!err.response.status) {
+      return;
+    }
+
+    switch (err.response.status) {
+      case HTTP_CODE.UNAUTHORIZED:
+        printError(MESSAGES.HTTP_UNAUTHORIZED);
+        break;
+      case HTTP_CODE.NOT_FOUND:
+        printError(MESSAGES.HTTP_NOT_FOUND);
+        break;
+      default:
+        printError(err.message);
+        break;
+    }
+  }
+};
+
 const initCLI = () => {
   const args = getObjectOfArgs(process.argv);
 
@@ -27,8 +51,13 @@ const initCLI = () => {
   if (args.t) {
     return saveToken(args.t);
   }
+  if (args.s) {
+    return saveToken(args.t);
+  }
 
-  getWeather('moscow');
+  const weather = getForcast();
+
+  console.log(weather);
 };
 
 export {initCLI};
