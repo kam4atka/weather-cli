f0f7122345a7b37d8b9b14adfc65128a676b6fda
kam4atka <sledsib@ya.ru>
2022-02-24 13:29:23 +0300

    Added API processing

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,8 @@
 import {getObjectOfArgs} from './helpers/args.js';
 import {printError, printHelp, printSuccess} from './services/logger.js';
 import {saveKeyValue} from './services/storage.js';
-import {MESSAGES} from './const.js';
+import {MESSAGES, ENTRIES} from './const.js';
+import {getWeather} from './services/api.js';
 
 const saveToken = async (token) => {
   if (!token.length) {
@@ -10,7 +11,7 @@ const saveToken = async (token) => {
   }
 
   try {
-    await saveKeyValue('token', token);
+    await saveKeyValue(ENTRIES.TOKEN, token);
     printSuccess(MESSAGES.TOKEN_SUCCESS);
   } catch (err) {
     printError(err.message);
@@ -26,6 +27,8 @@ const initCLI = () => {
   if (args.t) {
     return saveToken(args.t);
   }
+
+  getWeather('moscow');
 };
 
 export {initCLI};
